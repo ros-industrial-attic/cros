25d2eb12dabc4229036d3415d5108447b3402fbe
Francesco Pretto <francesco.pretto@it-robotics.it>
2014-03-21 16:08:35 +0100

    Added safeguard check on unsupported ai_family in lookup_host()

--- src/cros_api.c
+++ src/cros_api.c
@@ -25,7 +25,7 @@ lookup_host (const char *host, char *ip)
   struct addrinfo hints, *res;
   int errcode;
   char addrstr[100];
-  void *ptr;
+  void *ptr = NULL;
 
   memset (&hints, 0, sizeof (hints));
   hints.ai_family = PF_UNSPEC;
@@ -54,9 +54,17 @@ lookup_host (const char *host, char *ip)
           ptr = &((struct sockaddr_in6 *) res->ai_addr)->sin6_addr;
           break;
 #endif
+        default:
+          break;
         }
+      if (ptr == NULL)
+      {
+        PRINT_ERROR ("getaddrinfo unsupported ai_family");
+        return -1;
+      }
+
       inet_ntop (res->ai_family, ptr, addrstr, 100);
-      printf ("IPv%d address: %s (%s)\n", res->ai_family == PF_INET6 ? 6 : 4,
+      PRINT_VDEBUG ("IPv%d address: %s (%s)\n", res->ai_family == PF_INET6 ? 6 : 4,
               addrstr, res->ai_canonname);
       res = res->ai_next;
       strcpy(ip, (char*)&addrstr);
