2d20cf2d40ecef50f6e36263d85e6a257d93c037
Francesco Pretto <francesco.pretto@it-robotics.it>
2014-04-03 13:09:29 +0200

    Added missing signatures in cros_message.h
    Following signatures were missing:
     - cRosMessageParseServiceCallerHeader
     - cRosMessagePrepareServiceProviderHeader
     - cRosMessagePrepareServiceResponsePacket

--- include/cros_message.h
+++ include/cros_message.h
@@ -45,21 +45,21 @@ TcprosParserState cRosMessageParsePublicationHeader( CrosNode *n, int client_idx
 /*! \brief Prepare a TCPROS header to be initially sent to a publisher
  *
  *  \param n Ponter to the CrosNode object
- *  \param client_idx Index of the TcprosProcess ( tcpros_client_proc[server_idx] ) to be considered for the parsing
+ *  \param client_idx Index of the TcprosProcess ( tcpros_client_proc[server_idx] ) to be considered
  */
 void cRosMessagePrepareSubcriptionHeader( CrosNode *n, int client_idx );
 
 /*! \brief Prepare a TCPROS header to be initially sent back to a subscriber
  * 
  *  \param n Ponter to the CrosNode object
- *  \param server_idx Index of the TcprosProcess ( tcpros_server_proc[server_idx] ) to be considered for the parsing
+ *  \param server_idx Index of the TcprosProcess ( tcpros_server_proc[server_idx] ) to be considered
  */
 void cRosMessagePreparePublicationHeader( CrosNode *n, int server_idx );
 
 /*! \brief Prepare a TCPROS message (with data) to be sent to a subscriber
  * 
  *  \param n Ponter to the CrosNode object
- *  \param server_idx Index of the TcprosProcess ( tcpros_server_proc[server_idx] ) to be considered for the parsing
+ *  \param server_idx Index of the TcprosProcess ( tcpros_server_proc[server_idx] ) to be considered
  */
 void cRosMessagePreparePublicationPacket( CrosNode *n, int server_idx );
 
@@ -69,6 +69,31 @@ void cRosMessagePreparePublicationPacket( CrosNode *n, int server_idx );
  *  \param client_idx Index of the TcprosProcess ( tcpros_client_proc[server_idx] ) to be considered for the parsing
  */
 void cRosMessageParsePublicationPacket( CrosNode *n, int client_idx );
+
+/*! \brief Parse a RCPROS header sent from a service caller
+ *
+ *  \param n Ponter to the CrosNode object
+ *  \param client_idx Index of the TcprosProcess ( rpcros_server_proc[server_idx] ) to be considered for the parsing
+ *
+ *  \return Returns TCPROS_PARSER_DONE if the header is successfully parsed,
+ *          TCPROS_PARSER_HEADER_INCOMPLETE if the header is incomplete,
+ *          or TCPROS_PARSER_ERROR on failure
+ */
+TcprosParserState cRosMessageParseServiceCallerHeader( CrosNode *n, int server_idx);
+
+/*! \brief Prepare a RCPROS header to be initially sent back to a service caller
+ *
+ *  \param n Ponter to the CrosNode object
+ *  \param server_idx Index of the TcprosProcess ( rpcros_server_proc[server_idx] ) to be considered
+ */
+void cRosMessagePrepareServiceProviderHeader( CrosNode *n, int server_idx);
+
+/*! \brief Prepare a RCPROS response to be sent back to a service caller
+ *
+ *  \param n Ponter to the CrosNode object
+ *  \param server_idx Index of the TcprosProcess ( rpcros_server_proc[server_idx] ) to be considered
+ */
+void cRosMessagePrepareServiceResponsePacket( CrosNode *n, int server_idx);
 
 /*! @}*/
 
