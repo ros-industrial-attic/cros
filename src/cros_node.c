8ede4a154404b8c62e32c3939594eb9edae61bed
Francesco Pretto <francesco.pretto@it-robotics.it>
2014-04-11 12:49:08 +0200

    CROS-57 Fixed determinining publishing header size while reading tcpros packet

--- src/cros_node.c
+++ src/cros_node.c
@@ -419,7 +419,8 @@ static void doWithTcprosClientSocket( CrosNode *n, int client_idx)
           if (client_proc->left_to_recv == 0)
           {
             const unsigned char *data = dynBufferGetCurrentData(&client_proc->packet);
-            uint32_t header_size = (uint32_t)*data;
+            uint32_t header_size;
+            ROS_TO_HOST_UINT32(*((uint32_t *)data), header_size);
             tcprosProcessClear( client_proc, 0 );
             client_proc->left_to_recv = header_size;
             tcprosProcessChangeState( client_proc, TCPROS_PROCESS_STATE_READING_HEADER);
