c54e46caead6477ace43e52b20ee94a1744d088f
Richard R. Carrillo <rcarrillo@ugr.es>
2019-03-05 13:23:45 +0100

    Python source code for running the ROS Master has been added. This code has been extracted from a official ROS distribution. Now the cROS project contains all what is needed to create a ROS network.

---
+++ master_python_source/runmaster.py
@@ -0,0 +1,20 @@
+"""!@brief This Python script runs the ROS Master node, which is needed to use a ROS network.
+
+This script does not requires that a ROS distribution is installed.
+
+@date 5 Mar 2019
+"""
+
+#import sys
+#sys.path.append('.')
+import rosmaster
+
+ros_master=rosmaster.master.Master()
+ros_master.start()
+
+ros_master_ok=ros_master.ok()
+print("Master is running: {}".format(ros_master_ok))
+
+input("Press Enter to shutdown ROS Master and exit...")
+
+ros_master.stop()
